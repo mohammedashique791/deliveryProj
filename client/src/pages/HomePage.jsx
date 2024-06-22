2e79ba4db89adf2c3e09e7739fc497f7c6ed9f80
mohammedashique791 <mohammedashiquevt791@gmail.com>
2024-06-22 20:18:24 +0530

    Resolved navigation Problem, Now need to create food Adding mechanism.

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,15 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom";
 import axios from "axios";
 export default function Home(){
     const [result, setResult] = useState('');
+    const [food, setFood] = useState([]);
+
+    useEffect(()=>{
+        async function fetchFood(){
+
+        }
+    })
     async function createNewUser(){
         const {data} = await axios.get('')
     }
@@ -11,7 +18,7 @@ export default function Home(){
 
     return(
         <div>
-            <Link to={'/hotelAdding'} className="border p-3 rounded-xl bg-fonzzi">Add Hotel and Item</Link>
+            <Link to={'/hotelAdding'} className="border p-3 rounded-xl bg-fonzzi">Add Hotel</Link>
         </div>
     )
 }
